ccae47afe1d7a9fce97cf717a29fda7f1fb3d0d8
David-Moreira <david_nogueira_moreira@hotmail.com>
2020-11-21 00:14:29 +0000

    Add project files.

---
+++ DGridRenderHandle/Shared/DataGridWrapper.razor.cs
@@ -0,0 +1,17 @@
+﻿using Blazorise.DataGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGridRenderHandle.Shared
+{
+    public partial class DataGridWrapper<TItem> : DataGrid<TItem>
+    {
+
+        public DataGridWrapper()
+        {
+            base.PageSize = 20;
+        }
+    }
+}
